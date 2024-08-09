dd320bfb7bab160e75cf629f86d6ee83a26d6211
Liam <2217356256@qq.com>
2024-08-09 08:22:33 +0800

    A

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import Layout from './views/common/Layout.vue';
+</script>
+
+<template>
+
+    <Layout></Layout>
+
+ 
+</template>
+
+<style scoped>
+
+
+</style>
---
+++ src/views/common/Layout.vue
@@ -0,0 +1,23 @@
+<template>
+      <div >
+         <el-container>
+        <el-aside width="200px"><Asider></Asider></el-aside>
+              <el-main>无</el-main>
+       </el-container>
+  </div>
+</template>
+<script setup lang='ts'>
+import Asider from './Asider.vue';
+
+
+
+
+</script>
+<style scoped>
+
+.el-container{
+  height: 100vh;
+  
+
+}
+</style>
